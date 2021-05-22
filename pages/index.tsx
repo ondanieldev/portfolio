5d36e4db2c38be2640c05d85f3a2b84630423061
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2021-05-22 02:59:14 -0300

    feat: setup typescript

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
+import { NextPage } from 'next'
 import Head from 'next/head'
 
-export default function Home() {
+const Home: NextPage = () => {
   return (
     <div className="container">
       <Head>
@@ -207,3 +208,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default Home;
