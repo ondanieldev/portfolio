8f84ffb0020d17daa35108b6550d89dcf53d2df4
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2021-05-22 03:23:21 -0300

    feat: setup chakra ui

---
+++ components/Head/index.tsx
@@ -0,0 +1,17 @@
+import BaseHead from 'next/head'
+
+interface IProps {
+  title: string;
+  icon?: string;
+}
+
+const Head: React.FC<IProps> = ({ title, icon }) => {
+  return (
+    <BaseHead>
+      <title>{title}</title>
+      <link rel="icon" href={icon || "/favicon.ico"} />
+    </BaseHead>
+  )
+}
+
+export default Head;
