140d5cae1557713fe92c8ef43fccce73b21fd2b7
Daniel Oliveira <oficial.danieloliveira@gmail.com>
2021-07-04 04:05:09 -0300

    fix(presentation): add linkedin icon and reorder icons

--- src/components/Molecules/SocialBar/index.tsx
+++ src/components/Molecules/SocialBar/index.tsx
@@ -5,8 +5,9 @@ import {
   FaInstagram,
   FaGithub,
   FaTwitter,
-  FaTelegram,
+  FaTelegramPlane,
   FaEnvelope,
+  FaLinkedin,
 } from 'react-icons/fa';
 
 import CircularIcon from '../../Atoms/CircularIcon';
@@ -19,14 +20,15 @@ interface ISocialIcon {
 const SocialBar: React.FC = () => {
   const socials = useMemo<ISocialIcon[]>(
     () => [
+      { icon: FaEnvelope, link: 'mailto:me@ondaniel.com.br' },
+      { icon: FaGithub, link: 'https://github.com/stemDaniel' },
       {
         icon: FaInstagram,
         link: 'https://www.instagram.com/devdanieloliveira/',
       },
-      { icon: FaGithub, link: 'https://github.com/stemDaniel' },
+      { icon: FaLinkedin, link: 'https://www.linkedin.com/in/ondaniel/' },
+      { icon: FaTelegramPlane, link: 'https://t.me/ondaniel' },
       { icon: FaTwitter, link: 'https://github.com/stemDaniel' },
-      { icon: FaTelegram, link: 'https://t.me/ondaniel' },
-      { icon: FaEnvelope, link: 'mailto:me@ondaniel.com.br' },
     ],
     [],
   );
